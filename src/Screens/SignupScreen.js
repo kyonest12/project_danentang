589e8e5ad948cf4187b613f0ce640561fb3464c5
HaiquanZ <nn764562@gmail.com>
2024-01-10 22:43:21 +0700

    dieu khoan va dich vu

--- src/Screens/SignupScreen.js
+++ src/Screens/SignupScreen.js
@@ -454,12 +454,21 @@ export default function SignupScreen({ navigation }) {
                                     />
                                 </View>
                                 : stepIndex === 5 ?
-                                    <View style={{ flex: 1 }}>
+                                    <View style={{ flex: 1, alignItems: 'center' }}>
+                                        <Text style={{ color: 'blue', fontSize: 20, marginTop: 20}}>
+                                            Hoàn tất đăng ký
+                                        </Text>
+                                        <Text style={{marginLeft: 10, marginRight: 10, marginTop: 15}}>
+                                            Bằng cách nhấn vào Đăng ký, bạn đồng ý với Điều khoản, Chính sách dữ liệu và Chính sách cookie của chúng tôi.
+                                            Bạn có thể nhận dc thông báo của chúng tôi qua SMS và chọn không nhận bất cứ lúc nào. Thông tin từ danh bạ
+                                            của bạn sẽ được tải lên Facebook liên tục để chúng tôi có thể gợi ý bạn bè, cung cấp và cải thiện quảng cho bạn và người
+                                            khác, cũng như mang đến dịch vụ tốt hơn.
+                                        </Text>
                                         <View style={{ justifyContent: 'center', alignItems: 'center', marginHorizontal: 30 }}>
                                             <Button title="Đăng ký"
                                                 uppercase={false}
                                                 color={COMMON_COLOR.BLUE_COLOR}
-                                                style={{ width: '100%', position: 'absolute', top: height / 2, zIndex: 9999 }}
+                                                style={{ width: '100%', position: 'absolute', top: height / 3, zIndex: 9999 }}
                                                 onPress={() => handleSignUp()}
                                             />
                                         </View>
